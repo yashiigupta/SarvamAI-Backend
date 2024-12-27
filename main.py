8bd93ddc902bf052a3035dba5f42ea52c55560cb
Yashi Gupta <yashi.g23csai@nst.rishihood.edu.in>
2024-12-27 16:44:15 +0530

    user routes

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import os
+from routes.auth import router as auth_router
 from routes.chat import router as chat_router
 
 app = FastAPI()
@@ -16,7 +17,9 @@ app.add_middleware(
 )
 
 # Include routers
-app.include_router(chat_router, prefix="/api")
+app.include_router(chat_router, prefix="/api/v1", tags=["ai"])
+app.include_router(auth_router, prefix="/api/v1/auth", tags=["Auth"])
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
